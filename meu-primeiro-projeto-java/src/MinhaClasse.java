0c9f39a963e16ad1d29a41aa3c18a98f1c1b2031
Pedro Dutra <pedrodutra86@hotmail.com>
2025-01-07 21:45:07 -0300

    learning the basic about class and code

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+  public static void main (String [] args) {
+    String firstName = "Pedro";
+    String secondName = "Dutra";
+    
+    String fullName = fullName(firstName, secondName);
+    System.out.println(fullName);
+  }
+  public static String fullName (String firstName, String secondName) {
+    return firstName.concat(" ").concat(secondName);
+  }
+}
